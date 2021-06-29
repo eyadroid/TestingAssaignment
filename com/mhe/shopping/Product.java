40d2eb5087444a95af10555e86e3dd7395f0db15
Eyadroid <eyadismail.zen@gmail.com>
2021-06-29 13:14:30 +0200

    feat: products and search

--- com/mhe/shopping/Product.java
+++ com/mhe/shopping/Product.java
@@ -15,15 +15,88 @@ public class Product {
         this.stock = s;
     }
 
-    static Product[] products = {};
+    public Product(String i, String n, Category c, Market m, double p, int s) {
+        this.id = i;
+        this.name = n;
+        this.price = p;
+        this.category = c;
+        this.makert = m;
+        this.stock = s;
+    }
+
+    static Product[] products = {
+        new Product(
+            "1",
+            "Iphone 6+",
+            Category.categories[0],
+            Market.markets[0],
+            300,
+            1
+        ),
+        new Product(
+            "2",
+            "Redmi Note 8",
+            Category.categories[0],
+            Market.markets[0],
+            250,
+            1
+        ),
+
+        new Product(
+            "3",
+            "Hp Omen",
+            Category.categories[1],
+            Market.markets[0],
+            1000,
+            1
+        ),
+        new Product(
+            "4",
+            "Lenovo ThinkPad",
+            Category.categories[1],
+            Market.markets[1],
+            600,
+            1
+        ),
+    };
 }
 
 class Category {
     String id;
     String name;
+    public Category(String i, String n) {
+        this.id = i;
+        this.name = n;
+    }
+
+    public static Category[] categories = {
+        new Category(
+            "1",
+            "Phones"
+        ),
+        new Category(
+            "2",
+            "Laptops"
+        ),
+    };
 }
 
 class Market {
     String id;
     String name;
+    public Market(String i, String n) {
+        this.id = i;
+        this.name = n;
+    }
+
+    public static Market[] markets = {
+        new Market(
+            "1",
+            "China Market"
+        ),
+        new Market(
+            "2",
+            "USA Market"
+        ),
+    };
 }
--- com/mhe/shopping/Search.java
+++ com/mhe/shopping/Search.java
@@ -1,15 +1,38 @@
 package com.mhe.shopping;
 
+import java.util.ArrayList;
+
 public class Search {
     public Product[] searchByName(String name) {
-        return null;
+        ArrayList<Product> results = new ArrayList<Product>();
+        for (int i = 0; i < Product.products.length; i++) {
+            if (Product.products[i].name.matches(".*"+name+".*")) {
+                results.add(Product.products[i]);
+            }
+        }
+        
+        return results.toArray(new Product[0]);
     }
     
     public Product[] searchByCategory(String categoryId) {
-        return null;
+        ArrayList<Product> results = new ArrayList<Product>();
+        for (int i = 0; i < Product.products.length; i++) {
+            if (Product.products[i].category.id.equals(categoryId)) {
+                results.add(Product.products[i]);
+            }
+        }
+        
+        return results.toArray(new Product[0]);
     }
     
     public Product[] searchByMarket(String marketId) {
-        return null;
+        ArrayList<Product> results = new ArrayList<Product>();
+        for (int i = 0; i < Product.products.length; i++) {
+            if (Product.products[i].makert.id.equals(marketId)) {
+                results.add(Product.products[i]);
+            }
+        }
+        
+        return results.toArray(new Product[0]);
     }
 }
